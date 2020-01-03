f84e92229d0957f14ca73981d291a1cc7fef3276
lfc <18356288298@163.com>
2020-01-03 15:24:14 +0800

    1.0.0

---
+++ FCRolingNoticeView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FCRolingNoticeView
+//
+//  Created by ZupuMac on 2020/1/3.
+//  Copyright © 2020 LFC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
